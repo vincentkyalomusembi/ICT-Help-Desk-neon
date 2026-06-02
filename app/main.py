161825109eff8d19863305f599cf112453c8246d
SoonAnthony <anthonymedia062@gmail.com>
2026-06-02 11:23:30 +0300

    staff routes

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from app.core.database import check_db_connection
 from app.audit.routes import router as audit_router
+from app.staff.routes import router as staff_router
 
 
 
@@ -17,3 +18,4 @@ async def home():
     return {"message": "ICT Helpdesk API"}
 
 app.include_router(audit_router)
+app.include_router(staff_router)
