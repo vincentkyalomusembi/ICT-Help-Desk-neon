8e0c80d874af8a9fb733d594f4767ff3d911a13d
RaymondWachira <raymondkingoriwachira@gmail.com>
2026-06-03 09:26:58 +0300

    config update

--- app/core/config.py
+++ app/core/config.py
@@ -7,8 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 class Settings(BaseSettings):
     DATABASE_URL: str
     SECRET_KEY: str
-    ALGORITHM: str
-    SESSION_DURATION_HOURS: int
+    SESSION_EXPIRE_MINUTES: int
 
     model_config = SettingsConfigDict(
         env_file=str(BASE_DIR / ".env"),
