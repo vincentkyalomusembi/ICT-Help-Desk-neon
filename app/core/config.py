6aa77cf084a4951c1674cecd79419a27f5b9d24d
RaymondWachira <raymondkingoriwachira@gmail.com>
2026-06-03 09:18:19 +0300

    remove access token from config.py

--- app/core/config.py
+++ app/core/config.py
@@ -8,7 +8,6 @@ class Settings(BaseSettings):
     DATABASE_URL: str
     SECRET_KEY: str
     ALGORITHM: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int
     SESSION_DURATION_HOURS: int
 
     model_config = SettingsConfigDict(
